9497b00aa8f5604b8a1d86304a4e90c62a19617e
Xenos.exe <55803068+XenoS-ITA@users.noreply.github.com>
2021-08-04 18:33:12 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -6,43 +6,45 @@ Citizen.CreateThread(function()
 end)
 
 CreateLoop(function()
-    if IsPedReloading(PlayerPedId()) then -- If reloading hide the HUD
-        if n3d:visible() then
-            n3d:hide()
-        end
-    else
-        if IsControlPressed(0, 25) and IsPedArmed(PlayerPedId(), 4) then -- if is armed and is aiming
-            if not n3d:visible() then -- Show the HUD the first time
-                n3d:show(GetEntityCoords(PlayerPedId()))
-
-                local _, weaponHash = GetCurrentPedWeapon(PlayerPedId())
-                local obj = GetClosestObjectOfType(GetEntityCoords(PlayerPedId()), 2.0, GetWeapontypeModel(weaponHash))
-
-                n3d:attach(obj, vector3(0.6, 0.0, 0.7))
-                n3d:rotation(90.0, true)
-
-                local _, ammo = GetAmmoInClip(PlayerPedId(), weaponHash)
-                local maxammo = GetAmmoInPedWeapon(PlayerPedId(), weaponHash)
-
-                n3d:msg({
-                    ammo = ammo,
-                    maxammo = maxammo - ammo
-                })
-            end
-
-            if IsControlPressed(0, 24) then
-                local _, weaponHash = GetCurrentPedWeapon(PlayerPedId())
-                local _, ammo = GetAmmoInClip(PlayerPedId(), weaponHash)
-                local maxammo = GetAmmoInPedWeapon(PlayerPedId(), weaponHash)
-
-                n3d:msg({
-                    ammo = ammo,
-                    maxammo = maxammo - ammo
-                })
-            end
-        else
+    if n3d ~= nil then
+        if IsPedReloading(PlayerPedId()) then -- If reloading hide the HUD
             if n3d:visible() then
                 n3d:hide()
+            end
+        else
+            if IsControlPressed(0, 25) and IsPedArmed(PlayerPedId(), 4) then -- if is armed and is aiming
+                if not n3d:visible() then -- Show the HUD the first time
+                    n3d:show(GetEntityCoords(PlayerPedId()))
+
+                    local _, weaponHash = GetCurrentPedWeapon(PlayerPedId())
+                    local obj = GetClosestObjectOfType(GetEntityCoords(PlayerPedId()), 2.0, GetWeapontypeModel(weaponHash))
+
+                    n3d:attach(obj, vector3(0.6, 0.0, 0.7))
+                    n3d:rotation(90.0, true)
+
+                    local _, ammo = GetAmmoInClip(PlayerPedId(), weaponHash)
+                    local maxammo = GetAmmoInPedWeapon(PlayerPedId(), weaponHash)
+
+                    n3d:msg({
+                        ammo = ammo,
+                        maxammo = maxammo - ammo
+                    })
+                end
+
+                if IsControlPressed(0, 24) then
+                    local _, weaponHash = GetCurrentPedWeapon(PlayerPedId())
+                    local _, ammo = GetAmmoInClip(PlayerPedId(), weaponHash)
+                    local maxammo = GetAmmoInPedWeapon(PlayerPedId(), weaponHash)
+
+                    n3d:msg({
+                        ammo = ammo,
+                        maxammo = maxammo - ammo
+                    })
+                end
+            else
+                if n3d:visible() then
+                    n3d:hide()
+                end
             end
         end
     end
